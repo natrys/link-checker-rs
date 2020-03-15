ba59a1916c0419ea047b7d4a95b4c2ebb5f9f0f0
Imran Khan <imrankhan@teknik.io>
2020-03-15 17:58:10 +0600

    CLI argument parsing with clap for v1.0.0

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,30 @@ use std::time::Duration;
 use std::cell::RefCell;
 
 use anyhow::Result;
+use clap::Clap;
 use futures::future;
 use futures::stream::StreamExt;
-use reqwest::header;
+use reqwest::{header, Client};
+
+#[derive(Clap)]
+#[clap(version = env!("CARGO_PKG_VERSION"), author = "Imran Khan")]
+struct Config {
+    #[clap(
+        short = "u",
+        long = "user-agent",
+        default_value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+    )]
+    user_agent: String,
+    #[clap(
+        short = "t",
+        long = "timeout",
+        default_value = "10",
+        parse(try_from_str)
+    )]
+    timeout: u64,
+}
 
 thread_local!(
-    static CL: RefCell<reqwest::Client> = RefCell::new({
-        let mut headers = header::HeaderMap::new();
-        headers.insert("Accept", header::HeaderValue::from_static("text/html"));
-        reqwest::ClientBuilder::new()
-            .use_rustls_tls()
-            .default_headers(headers)
-            .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36")
-            .timeout(Duration::new(10, 0))
-            .build()
-            .unwrap()
-    });
-
     static PROGRESS: RefCell<usize> = RefCell::new(0);
 );
 
@@ -34,14 +41,16 @@ fn print_progress(total: usize) {
     });
 }
 
-async fn check(url: &str) -> Result<bool> {
-    let res = CL.with(|client| client.borrow().get(url).send()).await?;
+async fn check(client: &Client, url: &str) -> Result<bool> {
+    let res = client.get(url).send().await?;
     // eprintln!("{} {}", res.status(), url);
     Ok(res.status() == 200)
 }
 
 #[tokio::main]
 async fn main() {
+    let config = Config::parse();
+
     let urls = io::stdin()
         .lock()
         .lines()
@@ -49,9 +58,23 @@ async fn main() {
         .collect::<Vec<String>>();
     let total = urls.len();
 
+    let client = {
+        let mut headers = header::HeaderMap::new();
+        headers.insert("Accept", header::HeaderValue::from_static("text/html"));
+        reqwest::ClientBuilder::new()
+            .use_rustls_tls()
+            .default_headers(headers)
+            .user_agent(config.user_agent)
+            .timeout(Duration::from_secs(config.timeout))
+            .build()
+            .unwrap()
+    };
+
+    let cl = &client;
     futures::stream::iter(urls.iter().map(|url| async move {
-        let status = check(url).await;
+        let status = check(cl, url).await;
         print_progress(total);
+
         match status {
             Ok(true) => (true, url),
             _ => (false, url),
@@ -64,4 +87,6 @@ async fn main() {
         future::ready(())
     })
     .await;
+
+    eprintln!("");
 }
